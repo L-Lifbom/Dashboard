4ac2c922facca8efc682c9148a723df9e9fea13e
L-Lifbom <141008398+L-Lifbom@users.noreply.github.com>
2024-01-17 13:27:42 +0100

    fetching data from weather api

--- modules/weather.js
+++ modules/weather.js
@@ -1,14 +1,30 @@
 import axios from "axios";
 
-const url = 'http://api.weatherapi.com/v1/forecast.json?key=0c6b3ff93d2a4572830112130241701&q=London&days=7&aqi=no&alerts=no';
+let city = "Stockholm"
+let weatherToday = document.querySelector('.weather-today')
 
-async function getWeather(url) {
+async function getWeather() {
+    const apiKey = ""
+    let url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=7&lang=en`;
+
     try {
         const response = await axios.get(url);
-        console.log(response);
+        const weatherData = response.data
+
+        const temp = weatherData.current.temp_c;
+        const icon = weatherData.current.condition.icon;
+        const text = weatherData.current.condition.text;
+        console.log("Detta är current:" + weatherData.current.temp_c)
+
+        let weatherHTML = 
+        `<img src="${icon}" alt="Todays weather">
+        <p>${text}</p>
+        <p>${temp} C</p>`;
+        weatherToday.innerHTML = weatherHTML
+
     } catch (error) {
         console.log("Todays weather did not fetch correctly:",error);
     }
 }
 
-getWeather(url);
+getWeather();
